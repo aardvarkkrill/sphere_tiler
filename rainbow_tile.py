7660d70f542af0f3a8529067269f7bc2a2caee13
Andrew Kay <ubuntu@wildruby.co.uk>
2025-01-05 12:25:40 +0000

    WIP: shadow on pink

--- rainbow_tile.py
+++ rainbow_tile.py
@@ -2,13 +2,12 @@ import sys
 import math
 
 import pygame
-from typing import Tuple, Optional, List
+from typing import Union
 from collections.abc import Callable
 
 from sympy import ceiling
-from sympy.core.numbers import Infinity
-
-from brain_tile import create_canvas, create_hexagonal_mask, show_canvas
+from brain_tile import create_canvas
+import show_canvas
 
 #        2___1      ^
 #       /     \     |
@@ -21,8 +20,10 @@ from brain_tile import create_canvas, create_hexagonal_mask, show_canvas
 Point = pygame.math.Vector2
 Vertices = list[Point]
 
-# maps [0,1] to some kind of rainbow
-ColourFunction = Callable[[float], pygame.Color]
+# a colour, or [0,1]->colour
+ColourFunction = Union[pygame.Color, Callable[[float], pygame.Color]]
+# a colour, or [0,1]->colour, or [0,1]->[0,1]->colour
+MetaColourFunction = Union[ColourFunction, Callable[[float], ColourFunction]]
 
 
 class Polygon(object):
@@ -50,17 +51,19 @@ def angle(a: Point, b: Point) -> float:
 
 
 def arc(surface: pygame.Surface, cxy: Point, radius: float, theta0, theta1,
-        colour: pygame.Color, width: int):
+        colour: ColourFunction, width: int):
     """ draws an arc of a circle of given centre and radius between two angles.
         The drawing is always anti-clockwise from theta0 to theta1.
+        If a colour function is given, its parameter is the fraction of the arc
+            being drawn.
     """
 
     if theta0 > theta1:
         theta1 += 2 * math.pi
 
-    def point(theta):
-        return cxy + radius * pygame.math.Vector2(math.cos(theta),
-                                                  -math.sin(theta))
+    def point(theta_):
+        return cxy + radius * pygame.math.Vector2(math.cos(theta_),
+                                                  -math.sin(theta_))
 
     if radius < 3:
         # approx 1 step per 45 degrees.
@@ -73,13 +76,16 @@ def arc(surface: pygame.Surface, cxy: Point, radius: float, theta0, theta1,
     for i in range(steps):
         theta = theta0 + i * delta
         p1 = point(theta)
-        pygame.draw.line(surface, colour, p0, p1, width)
+        if callable(colour):
+            pygame.draw.line(surface, colour(i / steps), p0, p1, width)
+        else:
+            pygame.draw.line(surface, colour, p0, p1, width)
         p0 = p1
 
 
 def rainbow_arc(surface: pygame.Surface, polygon: Polygon,
                 start: int, finish: int,
-                colourfun: ColourFunction,
+                colourfun: MetaColourFunction,
                 extent: float = 0.6,
                 overshoot: float = 0.01) -> None:
     """ Draws a ribbon of arcs between polygon sides of index start and finish,
@@ -87,6 +93,8 @@ def rainbow_arc(surface: pygame.Surface, polygon: Polygon,
         The arcs meet the edges perpendicularly.
         ColourFunction is called with parameter 0 at the outsides of the ribbon,
              1 at the centre (so the ribbon colours are symmetric.)
+             Additionally, a MetaColourFunction is called with a parameter
+             denoting fraction along the arc being drawn
         The arc overshoots the edges by overshoot radians.
         NOTE: currently implemented only for hexagons
     """
@@ -95,7 +103,6 @@ def rainbow_arc(surface: pygame.Surface, polygon: Polygon,
     count = round(polygon.side * extent)
 
     distance = (finish - start) % N
-    even = distance % 2 == 0
 
     line = (distance == 3)
     centre = PerimeterPoint(start + [0, 1, 0, 2, 5, 0][distance],
@@ -106,8 +113,13 @@ def rainbow_arc(surface: pygame.Surface, polygon: Polygon,
         p2 = 1 - p1
         point_a = PerimeterPoint(start, p1).to_point(polygon)
         point_b = PerimeterPoint(finish, p2).to_point(polygon)
-        colour = colourfun(
-            fraction * 2 if fraction < 0.5 else (1 - fraction) * 2)
+
+        if callable(colourfun):
+            colour = colourfun(
+                fraction * 2 if fraction < 0.5 else (1 - fraction) * 2)
+        else:
+            colour = colourfun
+
         if line:
             pygame.draw.line(surface, colour, point_a, point_b, 2)
         else:
@@ -144,6 +156,15 @@ def rainbow_tile(extent: float = 0.6) -> pygame.Surface:
     return tile
 
 
+def blend_colours(a: pygame.Color, b: pygame.Color,
+                  alpha: float) -> pygame.Color:
+    def blend(c1, c2): return round(c1 * (1 - alpha) + c2 * alpha)
+    return pygame.Color(blend(a.r, b.r),
+                        blend(a.g, b.g),
+                        blend(a.b, b.b),
+                        blend(a.a, b.a))
+
+
 def pink_tile() -> pygame.Surface:
     """ creates the pink tile
         TODO: add the crossover shading
@@ -158,10 +179,17 @@ def pink_tile() -> pygame.Surface:
     #     pygame.draw.circle(tile, pygame.color.THECOLORS["black"], p, 5)
     # show_canvas.show_canvas(tile)
 
-    def pink(fraction: float) -> pygame.Color:
-        return pygame.Color(255, 0, 255, 255)
+    pink = pygame.Color(255, 0, 255, 255)
 
-    rainbow_arc(tile, polygon, 0, 2, pink, extent=0.3)
+    def pink_shade(fraction: float) -> ColourFunction:
+        def f(arc_fraction: float) -> pygame.Color:
+            amount = math.exp(- (arc_fraction - 0.35) ** 2)
+            return blend_colours(pygame.Color(255, 0, 255, 255),
+                                 pygame.Color(0, 0, 0, 255),
+                                 amount)
+        return f
+
+    rainbow_arc(tile, polygon, 0, 2, pink_shade, extent=0.3)
     rainbow_arc(tile, polygon, 1, 3, pink, extent=0.3)
     rainbow_arc(tile, polygon, 4, 5, pink, extent=0.3)
 
@@ -170,17 +198,21 @@ def pink_tile() -> pygame.Surface:
 
 if __name__ == "__main__":
 
-    tile_name = sys.argv[1] if len(sys.argv) > 1 else "pink"
+    def main():
+        tile_name = sys.argv[1] if len(sys.argv) > 1 else "pink"
 
-    if tile_name == "rainbow":
-        tile = rainbow_tile()
-        pygame.image.save(tile, "rainbow_tile.png")
-    elif tile_name == "pink":
-        tile = pink_tile()
-        pygame.image.save(tile, "pink_tile_tmp.png")
-    else:
-        print(f"""I don't know how to make tile \"{tile_name}\"\n
-                  Known tiles: rainbow, pink.""", file=sys.stderr)
-        exit(1)
+        if tile_name == "rainbow":
+            tile = rainbow_tile()
+            pygame.image.save(tile, "rainbow_tile.png")
+        elif tile_name == "pink":
+            tile = pink_tile()
+            pygame.image.save(tile, "pink_tile_tmp.png")
+        else:
+            print(f"""I don't know how to make tile \"{tile_name}\"\n
+                      Known tiles: rainbow, pink.""", file=sys.stderr)
+            exit(1)
 
-    show_canvas.show_canvas(tile)
+        show_canvas.show_canvas(tile)
+
+
+    main()
