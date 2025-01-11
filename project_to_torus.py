7b57bedb993a843d81314abfccecf2650f969669
Andrew Kay <ubuntu@wildruby.co.uk>
2025-01-11 15:37:18 +0000

    WIP more trying to project_to_torus.py

--- project_to_torus.py
+++ project_to_torus.py
@@ -8,6 +8,34 @@ from sympy import ceiling
 import numpy
 
 
+def rotate_x(theta: float) -> numpy.ndarray:
+    """ 4x4 matrix for rotating around the x-axis by theta radians."""
+    return numpy.array([
+        [1, 0, 0, 0],
+        [0, math.cos(theta), -math.sin(theta), 0],
+        [0, math.sin(theta), math.cos(theta), 0],
+        [0, 0, 0, 1]
+    ], dtype=numpy.float32)
+
+
+def translate(x: float, y: float, z: float) -> numpy.ndarray:
+    """ 4x4 matrix for translating the world."""
+    return numpy.array([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1]
+    ], dtype=numpy.float32)
+
+
+def NDC_to_raster_matrix(width: int, height: int) -> numpy.ndarray:
+    """ 2x3 projects [-1, 1] x [-1, 1] into the image region """
+    return numpy.array([
+        [width / 2, 0, width / 2],
+        [0, -height / 2, height / 2]
+    ], dtype=numpy.float32)
+
+
 def project_image_to_torus(
         output_size: Tuple[int, int],
         plane: pygame.Surface,
@@ -20,62 +48,56 @@ def project_image_to_torus(
     """
 
     # input plane (u, v)
-    u_width, u_height = plane.get_size()
+    uv_width, uv_height = plane.get_size()
 
     # torus (in model space, x,y,z)
-    rh = (u_height - 1) / (2 * math.pi)
-    rw = (u_width - 1) / (2 * math.pi)
+    rw = (uv_width - 1) / (2 * math.pi)
+    rh = (uv_height - 1) / (2 * math.pi)
+
+    camera_matrix = translate(0, 0, z=(rh + rw)) @ rotate_x(math.radians(30))
+    render_matrix = NDC_to_raster_matrix(*output_size)
 
     # output plane (X, Y)
+    how, hoh = output_size[0] / 2, output_size[1] / 2
     layer = pygame.Surface(size=output_size, flags=pygame.SRCALPHA)
     layer.fill((255, 255, 255, 255))
-    ow, oh = output_size
-    scale = min(ow, oh) / 2
-    ocx, ocy = ow / 2, oh / 2
 
     for theta in numpy.linspace(0, 2 * math.pi, max(*output_size) * 4):
         progress = theta / (2 * math.pi) * 100  # Calculate progress percentage
         sys.stdout.write(
-            f"\rToruse wrapping Progress: {progress:.0f}%")  # Overwrite the progress line
+            f"\rTorus wrapping Progress: {progress:.0f}%")  # Overwrite the progress line
         sys.stdout.flush()  # Ensure the progress line gets updated immediately
 
+        stheta, ctheta = math.sin(theta), math.cos(theta)
         for phi in numpy.linspace(0, 2 * math.pi, max(*output_size) * 4):
-            u, v = round(rh * phi), round(rw * theta)
+            u, v = round(rw * theta), round(rh * phi)
             pixel_colour = plane.get_at((u, v))
 
-            # position in model space [-1, +1]^3
+            # pixel is fully transparent
+            if pixel_colour[3] == 0:
+                continue
+
+            # position in model space
             #       _____
-            #      /     \     y towards viewer
+            #      /     \     z towards viewer
             #     |   O   |    ---> x
             #      \     /     |
-            #       -----      v z
-            x = ((rw + rh * math.cos(phi) * math.cos(theta)) - rw) / (
-                        2 * (rh + rw))
-            y = ((rw + rh * math.cos(phi) * math.sin(theta)) - rw) / (
-                        2 * (rh + rw))
-            z = - math.sin(phi)
+            #       -----      v y
+            cphi = math.cos(phi)
+            x = (rw + rh * cphi) * ctheta
+            y = (rw + rh * cphi) * stheta
+            z = rh * math.sin(phi)
 
-            # Viewer distance and tilt angle
-            viewer = 2.0  # Distance of viewer (y-coordinate) from the origin
-            alpha = math.radians(30)  # Tilt angle in radians
-
-            # Apply tilt by rotating around the x-axis
-            z_tilted = z * math.cos(alpha) - y * math.sin(alpha)
-            y_tilted = z * math.sin(alpha) + y * math.cos(alpha)
-
-            # Compute the perspective projection for the X, Y plane
-            fn = scale * viewer / (viewer + y_tilted)  # Perspective factor
-            X = int(clamp(ocx + x * fn, 0, ow - 1))  # Map to [0, ow]
-            Y = int(clamp(ocy + z_tilted * fn, 0, oh - 1))  # Map to [0, oh]
-
-            # Set the pixel
-            if pixel_colour[3] > 0:
-                layer.set_at((X, Y), pixel_colour)
+            X, Y, Z = (camera_matrix @ [x, y, z, 1])[:3]
+            sx, sy = round(how + X * how / Z), round(hoh - Y * hoh)
+            if 0 <= sx < output_size[0] and 0 <= sy < output_size[1]:
+                # Set the pixel
+                if pixel_colour[3] > 0:
+                    layer.set_at((sx, sy), pixel_colour)
 
     sys.stdout.write("\r")  # Clear the progress line
     sys.stdout.flush()  # Ensure the progress line gets updated immediately
     return layer
-
 
 # Usage example
 if __name__ == "__main__":
@@ -85,7 +107,8 @@ if __name__ == "__main__":
         shade = 0.3 if len(sys.argv) <= 3 else float(sys.argv[3])
 
 
-    plane_file = "leafy_plane_v3.png"
+    # plane_file = "leafy_plane_v3.png"
+    plane_file = "amazon_crop.jpg"
     plane = pygame.image.load(plane_file)
 
     torus = project_image_to_torus((400, 400), plane)
