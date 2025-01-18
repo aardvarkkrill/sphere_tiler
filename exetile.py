f75fc34910b1b7695dec0bf957ccab78f02c5065
Andrew Kay <ubuntu@wildruby.co.uk>
2025-01-18 15:42:29 +0000

    WIP -- calculating how much ribbon sampling is needed.

--- exetile.py
+++ exetile.py
@@ -11,13 +11,31 @@ class LineArtist(object):
     def stroke(self, a: Point2, b: Point2, colour: pygame.Color, width: int):
         pass
 
+    # given 2D points as row vectors, converts each one in to camera space as
+    # 3D row vectors
+    def model_to_camera_space(self, points2d: numpy.ndarray) -> numpy.ndarray:
+        pass
+
+    # given 3D points in camera space, converts each one in to image space as
+    # 2D row vectors of pixel coordinates
+    def camera_to_image_space(self, points3d: numpy.ndarray) -> numpy.ndarray:
+        pass
+
 
 class PlaneLineArtist(LineArtist):
     def __init__(self, surface: pygame.Surface):
         self.surface = surface
+        self.centre = numpy.array((surface.get_width() / 2,
+                                   surface.get_height() / 2))
 
     def stroke(self, a: Point2, b: Point2, colour: pygame.Color, width: int):
         pygame.draw.line(self.surface, colour, a, b, width)
+
+    def model_to_camera_space(self, points2d: numpy.ndarray) -> numpy.ndarray:
+        return numpy.hstack((points2d, numpy.ones((points2d.shape[0], 1))))
+
+    def camera_to_image_space(self, points3d: numpy.ndarray) -> numpy.ndarray:
+        return points3d[:, :-1] * self.centre[0] + self.centre
 
 
 BMatrix = numpy.array([[1, 0, 0, 0],
@@ -73,7 +91,8 @@ class Tile(object):
         """ draw the tile given a projection matrix to image space. """
         for i in range(self.N):
             line_artist.stroke(a=self.do_transform(self.vertices[i], transform),
-                               b=self.do_transform(self.vertices[(i + 1) % self.N], transform),
+                               b=self.do_transform(
+                                   self.vertices[(i + 1) % self.N], transform),
                                colour=pygame.Color(
                                    pygame.color.THECOLORS['green']),
                                width=2)
@@ -87,12 +106,24 @@ class Tile(object):
             n2 = self.edge_normal(a2, b2)
             centre = self.branch_points[branch_index][0]
             halfwidth = self.branch_points[branch_index][1] * 0.5
-            for branch_fraction in numpy.linspace(centre - halfwidth,
-                                                  centre + halfwidth, 10):
+
+            def fraction_to_control_points_model(fraction: float):
                 P1 = a1 + (b1 - a1) * branch_fraction
                 P2 = a2 + (b2 - a2) * (1.0 - branch_fraction)
                 N1 = P1 + n1 * numpy.linalg.norm(b1 - a1) * normal_scale
                 N2 = P2 + n2 * numpy.linalg.norm(b2 - a2) * normal_scale
+                return P1, N1, N2, P2
+
+            # find the length of the base lines in pixels, and choose the max,
+            # to estimate the number of samples required
+            px_s = fraction_to_control_points_model(centre - halfwidth)
+            px_f = fraction_to_control_points_model(centre + halfwidth)
+            samples = 2 + 2 * max(numpy.linalg.norm(px_s[0] - px_s[3]),
+                                  numpy.linalg.norm(px_f[0] - px_f[3]))
+            for branch_fraction in numpy.linspace(centre - halfwidth,
+                                                  centre + halfwidth, samples):
+                control_points = fraction_to_control_points_model(
+                    branch_fraction)
                 ts = numpy.linspace(0.0, 1.0, 50)
                 bs = bezier(P1, N1, N2, P2, ts)
                 b0 = bs[0]
@@ -104,7 +135,8 @@ class Tile(object):
                   transform: Optional[numpy.ndarray] = None) -> Tuple[
         Point2, Point2]:
         """ the points defining the edge of given index [0..N-1] """
-        a, b = self.vertices[edge_index], self.vertices[(edge_index + 1) % self.N]
+        a, b = self.vertices[edge_index], self.vertices[
+            (edge_index + 1) % self.N]
         if transform is None:
             return a, b
         else:
@@ -137,7 +169,7 @@ T1 = Tile(vertices=vertices,
 s = pygame.Surface((500, 500))
 s.fill(color=pygame.Color(pygame.color.THECOLORS['black']))
 T1.draw(PlaneLineArtist(s), pygame.Color(pygame.color.THECOLORS['pink']),
-        numpy.array([[0.5, 0,0],[0, 0.5,0],[0,0,1]], numpy.float32))
+        numpy.array([[0.5, 0, 0], [0, 0.5, 0], [0, 0, 1]], numpy.float32))
 
 # s = pygame.Surface((500, 500), flags=pygame.SRCALPHA)
 # s.fill(pygame.Color(0, 0, 0, 255))
